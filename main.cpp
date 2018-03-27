c13c22afe2bedd98a2557259f1bf61b5b2fa02bd
legible01 <legible01@gmail.com>
2018-03-27 04:14:55 -0400

    basic file WIPS

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "pktpassway.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //anyway. this part is one of
+
+   // if(correctDev)
+
+
+    return 0;
+}
---
+++ pktpassway.h
@@ -0,0 +1,4 @@
+#ifndef PKTPASSWAY_H
+#define PKTPASSWAY_H
+
+#endif // PKTPASSWAY_H
